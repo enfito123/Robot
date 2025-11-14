6d81752be11c1549ac933cb4ac42faf858c44a8a
enfito123 <danimetin@hotmail.com>
2025-11-14 14:06:52 +0100

    Termino la clase Coordenada.java

---
+++ src/main/java/org/iesalandalus/programacion/robot/modelo/Orientacion.java
@@ -0,0 +1,12 @@
+package org.iesalandalus.programacion.robot.modelo;
+
+public enum Orientacion {
+    NORTE ("Norte"),
+    NORESTE ("Noreste"),
+    ESTE ("Este"),
+    SURESTE ("Sureste"),
+    SUR ("Sur"),
+    SUROESTE ("Suroeste"),
+    OESTE ("Oeste"),
+    NOROESTE ("Noroeste");
+}
